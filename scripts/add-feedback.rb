d4123bac71d1ce3b7c93548305222d9a6cad607a
gv0tch0 <nkolev@gmail.com>
2009-04-15 17:47:00 -0400

    script to add the feedback widget to the static content

---
+++ scripts/add-feedback.rb
@@ -0,0 +1,43 @@
+require 'find'
+
+cmd = File.basename $0
+usage = <<USAGE_MESSAGE
+usage: #{cmd} top-level-directory
+         top-level-directory -- the directory which will be recursively
+                                searched for html files to which the
+                                feedback widget will be added
+USAGE_MESSAGE
+
+if ARGV.size != 1
+  print usage
+  exit 1
+end
+
+feedback = <<FEEDBACK_WIDGET_END
+    <style type='text/css'>@import url('http://s3.amazonaws.com/getsatisfaction.com/feedback/feedback.css');</style>
+    <script src='http://s3.amazonaws.com/getsatisfaction.com/feedback/feedback.js' type='text/javascript'></script>
+    <script type=text/javascript charset=utf-8>
+      var tab_options = {}
+      tab_options.placement = "right";
+      tab_options.color = "#BFBD00";
+      GSFN.feedback('http://getsatisfaction.com/glass-print/feedback/topics/new?display=overlay&style=question', tab_options);
+    </script>
+  </body>
+FEEDBACK_WIDGET_END
+
+www_page_regex = Regexp.new '^.*\.html$'
+body_close_tag_regex = Regexp.new '^\s*</body>\s*$'
+
+Find.find "." do |path|
+  if www_page_regex.match path
+    source = File.new path
+    dest = File.new path + ".tmp", "w"
+    while line = source.gets
+      if body_close_tag_regex.match line
+        dest.puts feedback
+      else
+        dest.puts line
+      end
+    end
+  end
+end
