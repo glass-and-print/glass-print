630c0650af0fd7445fbb24df4a418fca8b342b38
Nik Kolev <gv0tch0@gmail.com>
2014-05-06 20:05:31 -0400

    It works...

--- generators/ps.py
+++ generators/ps.py
@@ -4,6 +4,56 @@ import glob
 import string
 import os
 import sys
+import bs4
+import re
+
+all_digits = re.compile('\d+')
+dim = re.compile('^\d+.*\s+x\s+.*\d+.*$')
+
+imgurl_format = 'http://glass-print.com/images/{0}/{1}/{2}'
+imgpath_format = '../../images/{0}/{1}/{2}.*'
+
+def path_to_image(typ, vendor, handle):
+    return os.path.basename(glob.glob(imgpath_format.format(typ, vendor, handle))[0])
+
+def quote(val):
+    return '"' + val + '"'
+
+def parse(fn):
+    ft = None
+    with open(fn, 'r') as f:
+        ft = bs4.BeautifulSoup(f)
+    return ft
+
+def normalize(tag):
+    import unicodedata
+    return unicodedata.normalize('NFKD', tag.text).encode('ascii','ignore').strip()
+
+def title(parent):
+    title = parent.find('td', id='piece-title')
+    if title:
+        title = normalize(title)
+    return title
+
+def price(parent):
+    price = parent.select(".product-price")
+    if price and len(price) > 0:
+        price = price[0]['value']
+        if all_digits.match(price):
+            return price
+    return None
+
+def body(parent):
+    result = ""
+    for desc_tag in t.find_all('td', colspan='2'):
+        desc = normalize(desc_tag)
+        if desc:
+            if dim.match(desc):
+                desc = desc.replace('"', ' in')
+            else:
+                desc = desc.replace('"', '')
+            result = result + desc + " <br />"
+    return result
 
 COL_HANDLE = 'Handle'
 COL_TITLE = 'Title'
@@ -27,6 +77,13 @@ header = [ COL_HANDLE,
            COL_IMGURL,
            COL_IMGTXT ]
 
+COL_HANDLE_IDX = header.index(COL_HANDLE)
+COL_TITLE_IDX = header.index(COL_TITLE)
+COL_BODY_IDX = header.index(COL_BODY)
+COL_PRICE_IDX = header.index(COL_PRICE)
+COL_IMGURL_IDX = header.index(COL_IMGURL)
+COL_IMGTXT_IDX = header.index(COL_IMGTXT)
+
 SEGMENT_POSTERS = 'vintage-posters'
 TYP_POSTER = 'poster'
 SEGMENT_GLASS = 'art-glass'
@@ -35,7 +92,7 @@ TYP_GLASS = 'glass'
 TYP_2_SEGMENT = { TYP_POSTER : SEGMENT_POSTERS,
                   TYP_GLASS : SEGMENT_GLASS }
 
-def deterime_type(cwd):
+def determine_type(cwd):
     typ = 'unknown'
     if SEGMENT_POSTERS in cwd:
         return TYP_POSTER
@@ -65,23 +122,25 @@ record = [ None,
            None,
            None ]
 
-imgurl_format ='http://glass-print.com/images/{0}/{1}/{2}.jpg'
-
-def tx_title(handle):
-    segs = handle.split('-')
-    tx_segs = []
-    for seg in segs:
-        tx_segs.append(seg[0].upper() + seg[1:])
-    return string.join(tx_segs, ' ')
-
-print string.join(header, ',')
+print ','.join(header)
 for f in glob.glob('*.html'):
-    if f == 'all.html':
+    if f == 'all.html' or f == 'index.html':
         continue
-
     handle = f.split('.')[0]
-
-    record[header.index(COL_HANDLE)] = handle
-
-    print string.join(record, ',')
+    t = parse(f).find('table', id='piece-description')
+    if t:
+        pric = price(t)
+        titl = title(t)
+        bod = body(t)
+        if pric and titl and bod:
+            record[COL_HANDLE_IDX] = quote(handle)
+            record[COL_TITLE_IDX] = quote(titl)
+            record[COL_BODY_IDX] = quote(bod)
+            record[COL_PRICE_IDX] = quote(pric)
+            ts = TYP_2_SEGMENT[typ]
+            record[COL_IMGURL_IDX] = quote(imgurl_format.format(ts,
+                                                                vendor,
+                                                                path_to_image(ts, vendor, handle)))
+            record[COL_IMGTXT_IDX] = quote(titl)
+            print ','.join(record)
 
